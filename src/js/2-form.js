ca25470794c4e05beafb78bd8048c511b36f12f6
Roman Hrydin <romario198901@gmail.com>
2025-11-09 16:12:48 +0200

    complete task2 from the homework

---
+++ src/js/2-form.js
@@ -0,0 +1,31 @@
+const formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', e => {
+  if (e.target.name === 'email') {
+    formData.email = e.target.value.trim();
+  } else {
+    formData.message = e.target.value.trim();
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+const savedData = localStorage.getItem('feedback-form-state');
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  form.elements.email.value = parsedData.email || '';
+  form.elements.message.value = parsedData.message || '';
+}
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+  if (email.trim() === '' || message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+});
